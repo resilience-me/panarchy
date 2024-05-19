54ad8b697cb3acc2a99a4c1c72c5a656c1c42662
resilience-me <emailsareobsolete@gmail.com>
2024-05-19 13:54:01 +0200

    Create Rewards.sol

---
+++ Rewards.sol
@@ -0,0 +1,65 @@
+// Will likely be adding ability to automate validator rewards to voters, current work on that in code below. Was previously 
+// skipping it and leaving it to be done manually, mostly because the Geth consensus engine interface seemed to not support it well 
+// (was no way to read state during prepare for example) but found a few good workarounds, one relies on temporary rewarad storage contracts
+// that can be deterministically known (thus hardcoded into Geth) and it seems like a good solution so I'll code that at least, and possibly
+// restart the network to use it
+
+contract Bitpeople { function seed(uint t) external view returns (uint) {} }
+
+contract Election {
+    function election(uint t, uint i) external view returns (address) {}
+    function electionLength(uint t) external view returns (uint) {}
+}
+
+contract Reward {
+    address constant internal electionContract = 0x0000000000000000000000000000000000000011;
+    function withdraw(address rewardAddress) external { require(msg.sender == electionContract); selfdestruct(payable(rewardAddress)); }
+}
+
+contract Schedule {
+
+    uint constant public genesis = 1715407200;
+    uint constant public period = 4 weeks;
+
+    function schedule() public view returns(uint) { return ((block.timestamp - genesis) / period); }
+}
+
+contract Rewards is Schedule {
+
+    Bitpeople bitpeople = Bitpeople(0x0000000000000000000000000000000000000010);
+    Election election = Election(0x0000000000000000000000000000000000000011);
+
+    uint constant public slotTime = 12;
+    uint public nonce;
+
+    struct RewardContract {
+        address addr;
+        uint[] slotsRewarded;
+        uint rewardsClaimed;
+        uint validSince;
+    }
+    mapping (address => RewardContract[]) public rewardContract;
+
+    function createRewardContract() external returns (bool) {
+        uint256 currentSlot = (block.timestamp - genesis) / slotTime;
+        if(currentSlot <= nonce) return false;
+        Reward reward = new Reward();
+        if(address(reward).balance == 0) {
+            reward.withdraw(address(0));
+            return true;
+        }
+        uint t = (nonce * slotTime) / period;
+        uint i = (bitpeople.seed(t) + uint256(keccak256(abi.encode(nonce)))) % election.electionLength(t);
+        address validator = election.election(t, i);
+
+        uint rewardContractIndex = rewardContract[validator].length-1;
+        while(rewardContract[validator][rewardContractIndex].validSince > t) { rewardContractIndex--; }
+        if(rewardContract[validator][rewardContractIndex].addr == address(0)) {
+            reward.withdraw(validator);
+            return true;
+        }
+        rewardContract[validator][rewardContractIndex].slotsRewarded.push(nonce);
+        nonce++; // Increment the nonce after creating the contract
+        return true;
+    }
+}
