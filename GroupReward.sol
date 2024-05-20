6759ac7719669e735ad36312274fde89d19b1acf
resilience-me <emailsareobsolete@gmail.com>
2024-05-21 00:45:19 +0200

    Create GroupReward.sol

---
+++ GroupReward.sol
@@ -0,0 +1,32 @@
+// An example contract for how validator could manage group rewards. These reward contracts are custom and written or chosen by validators, 
+// the Panarchy system does not enforce any standard. I just provide this as an example (and anyone can use it of course. )
+
+contract Coinbase {
+    address public validator;
+    mapping(address => uint) public votes;
+    uint public totalVotes;
+    uint public balance;
+
+    constructor(address _validator) {
+        validator = _validator;
+    }
+
+    modifier onlyValidator() {
+        require(msg.sender == validator, "Only the validator can perform this action");
+        _;
+    }
+
+    function recordVote(address voter) external onlyValidator {
+        votes[voter]++;
+        totalVotes++;
+    }
+
+    function claimReward(address voter) external onlyValidator {
+        if (balance == 0) { balance = address(this).balance; }
+
+        uint reward = (balance * votes[voter]) / totalVotes;
+        delete votes[voter];
+        (bool success, ) = voter.call{value: reward}("");
+        require(success, "Transfer failed.");
+    }
+}
