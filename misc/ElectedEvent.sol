8a22365699dd7f8ba6104d0b1e03e47ab47d6cb3
resilience-me <emailsareobsolete@gmail.com>
2024-05-21 22:08:53 +0200

    Update and rename CoinbaseEvent.sol to ElectedEvent.sol

--- misc/ElectedEvent.sol
+++ misc/ElectedEvent.sol
@@ -1,8 +1,3 @@
-// An idea for how to emit block reward events. The problem here is that it also emits for missed validator slots.
-// An easy workaround is to check balance of the coinbase, but then it will not emit for collected coinbases (which
-// seems a bit messy, either emit for all payouts or none... ) Alternatively, people can poll the coinbase contracts 
-// they are part of.
-
 interface Bitpeople { function seed(uint t) external view returns (uint); }
 
 interface Election {
@@ -12,14 +7,12 @@ interface Election {
 }
 
 contract Schedule {
-
     uint constant public genesis = 1715407200;
     uint constant public period = 4 weeks;
-
     function schedule() public view returns(uint) { return ((block.timestamp - genesis) / period); }
 }
 
-contract CoinbaseEvent is Schedule {
+contract ElectedEvent is Schedule {
 
     Bitpeople bitpeople = Bitpeople(0x0000000000000000000000000000000000000010);
     Election election = Election(0x0000000000000000000000000000000000000011);
@@ -27,15 +20,15 @@ contract CoinbaseEvent is Schedule {
     uint constant public slotTime = 12;
     uint public nonce;
 
-    event BlockReward(uint indexed schedule, uint indexed slot, address indexed coinbase);
+    event Elected(uint indexed slot, address indexed validator, address indexed coinbase);
 
-    function emitCoinbaseEvent() external returns (bool) {
-        uint256 currentSlot = (block.timestamp - genesis) / slotTime;
-        if(currentSlot <= nonce) return false;
+    function emitElectedEvent() external returns (bool) {
+        uint256 slot = (block.timestamp - genesis) / slotTime;
+        if(slot <= nonce) return false;
         uint t = nonce * slotTime / period;
         uint i = (bitpeople.seed(t) + uint256(keccak256(abi.encode(nonce)))) % election.votesLength(t);
-        address coinbase = election.votes(t, i).coinbase;
-        emit BlockReward(t, slot, coinbase);
+        Vote vote = election.votes(t, i);
+        emit Elected(slot, vote.validator, vote.coinbase);
         nonce++;
         return true;
     }
