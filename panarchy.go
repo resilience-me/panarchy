e5efa6e5ab72012cdc9c415a6fb3c92c3d904548
resilience-me <emailsareobsolete@gmail.com>
2024-05-22 00:06:30 +0200

    Update panarchy.go

--- panarchy.go
+++ panarchy.go
@@ -212,7 +212,7 @@ func (p *Panarchy) verifySealAndCoinbase(header *types.Header, parentHeader *typ
 	}
 
 	coinbase := p.getCoinbase(voteSlot, state)
-	if coinbase == common.Address{} {
+	if coinbase == (common.Address{}) {
 		coinbase = validator
 	}
 	if header.Coinbase != coinbase {
