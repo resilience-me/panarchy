6491566e1a08e1802e0853c8ff0e5648dfb40c97
resilience-me <emailsareobsolete@gmail.com>
2024-05-21 03:32:22 +0200

    Update SingleReward.sol

--- SingleReward.sol
+++ SingleReward.sol
@@ -28,3 +28,46 @@ contract Coinbase {
         require(validatorSuccess, "Validator transfer failed.");
     }
 }
+
+interface Election {
+    function schedule() external view returns (uint);
+    function vote(address validator, address coinbase) external;
+    function transferFrom(address from, address to, uint256 value) external;
+    function allowance(uint t, address owner, address spender) external view returns (uint);
+}
+
+contract SingleReward {
+    Election public election = Election(0x0000000000000000000000000000000000000011);
+
+    address public validator;
+    uint public voterShare;
+
+    mapping(address => Coinbase) public coinbase;
+
+    constructor(address _validator, uint _voterShare) {
+        validator = _validator;
+        voterShare = _voterShare;
+    }
+
+    modifier onlyValidator() {
+        require(msg.sender == validator, "Only the validator can perform this action");
+        _;
+    }
+
+    function vote(address voter) external {
+        uint t = election.schedule();
+        if (address(coinbase[voter]) == address(0)) {
+            coinbase[voter] = new Coinbase(address(this), voter, validator, voterShare);
+        }
+        Coinbase _coinbase = coinbase[voter];
+        require(election.allowance(t, voter, address(this)) >= 1, "Insufficient allowance to vote");
+        election.transferFrom(voter, address(this), 1);
+        election.vote(validator, address(_coinbase));
+    }
+
+    function claimReward() external {
+        Coinbase _coinbase = coinbase[msg.sender];
+        require(address(_coinbase) != address(0), "Coinbase not set for this voter");
+        _coinbase.claimReward();
+    }
+}
