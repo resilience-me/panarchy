6b9c11166ea0e37d7053659ae1d51c920c64172a
resilience-me <emailsareobsolete@gmail.com>
2024-05-24 02:48:32 +0200

    Update main.js

--- website/main.js
+++ website/main.js
@@ -121,7 +121,7 @@ async function fetchAccountInfo(address, bitpeople) {
         } else {
             handleOtherScenarios(address, data, isMetamask, bitpeople);
         }
-	if (hasBitpeopleTokens) {
+	if (helper.hasBitpeopleTokens) {
 	    createTransferDiv(data.contracts.bitpeople.currentData.account.tokens, bitpeople);
 	    appendOption("Transfer");
 	}
