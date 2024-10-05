ec70a63877645f0812fb6c3b4b043519f366aa66
resilience-me <emailsareobsolete@gmail.com>
2024-10-05 16:06:10 +0700

    Update Bitpeople.sol

--- Bitpeople.sol
+++ Bitpeople.sol
@@ -1,6 +1,6 @@
 contract Bitpeople {
 
-    uint constant public genesis = 1726293600;
+    uint constant public genesis = 1728108000;
     uint constant public period = 4 weeks;
 
     function schedule() public view returns(uint) { return (block.timestamp - genesis) / period; }
